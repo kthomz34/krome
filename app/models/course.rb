8cd68b5881cf1caf6cb1d61ba13ce95e8c9b2b97
Kurt Thomas <kthomz34@icloud.com>
2016-01-15 19:38:43 +0000

    credit card button

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
+	
 	belongs_to :user
 	has_many :sections
 	has_many :enrollments
@@ -6,6 +8,13 @@ class Course < ActiveRecord::Base
 	validates :title, :presence => true
 	validates :description, :presence => true
 	validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-
-	mount_uploader :image, ImageUploader
+	
+	def free?
+	  cost.zero?
+	end
+	
+	def premium?
+  	  ! free?
+  	end
+	
 end
